df45065b8f93811ed0a5f7c23c0f227afab80c9b
dimuthnc <dimuthcse@gmail.com>
2025-06-01 21:25:13 +0800

    adding semicolon for all use data statements

--- ec2-user-data.sh
+++ ec2-user-data.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 # Update the OS
-apt update -y && apt upgrade -y
+apt update -y;
 
 # Install Apache (httpd) and Git
-apt install -y apache2 git
+apt install -y apache2 git;
 
 # Enable and start Apache
-systemctl enable apache2
+systemctl enable apache2;
 
 
 # Clone the public GitHub repo
-cd /tmp
-git clone https://github.com/dimuthnc/caap.git
+cd /tmp;
+git clone https://github.com/dimuthnc/caap.git;
 
 # Copy the HTML file to the Apache web directory
-cp caap/index.html /var/www/html/index.html
+cp caap/index.html /var/www/html/index.html;
 
 # Set correct permissions
-chown www-data:www-data /var/www/html/index.html
-chmod 644 /var/www/html/index.html
-systemctl start apache2
+chown www-data:www-data /var/www/html/index.html;
+chmod 644 /var/www/html/index.html;
+systemctl start apache2;
